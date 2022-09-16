7709520c6d19f0dcd972646dee599e10066cafc4
lamaurgen <65845025+lamaurgen@users.noreply.github.com>
2022-09-16 17:48:31 +0545

    ReactPractice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import classes from './App.module.css'
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className={classes.topbar}>
+          <img src="./images/alibaba.png" alt="logo" />
+        </nav>
+        
       </header>
+      <div className={classes.whole}>
+        <div className={classes.left}>
+          <img src="./images/black.png" alt="blackwatch" />
+        </div>
+        <div className={classes.right}>
+          <h1>Apple Watch Series 8</h1>
+          <p>The Apple Watch Series 8 is $399 for Wi-Fi only, and $499 for cellular. Three months of Fitness+ is included. 
+            For comparison, the base model starts at the same price as Series 4 through 7.</p>
+        <div className={classes.bottom}>
+          <h3>Select Color</h3>
+          <div className={classes.products}>
+            <img src="./images/blue.png" alt="bluewatch" />
+            <img src="./images/black.png" alt="blackwatch" />
+            <img src="./images/purple.png" alt="purplewatch" />
+            <img src="./images/red.png" alt="redwatch" />
+          </div>
+          <button className={classes.btn}>Buy Now</button>
+        </div>
+            
+          
+        </div>
+      </div>
+      
+       
     </div>
   );
 }
